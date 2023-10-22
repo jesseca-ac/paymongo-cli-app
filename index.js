c3a2c7e3b72520c792628dff6c856b573f6370a3
jesseca-ac <jesseca.casanare@zapier.com>
2023-10-22 13:44:33 +0800

    creates/payment_method.js created using scaffold, api 6.1.1 aka paymongo cli added as dependency

---
+++ index.js
@@ -0,0 +1,21 @@
+const getPayment = require("./triggers/payment");
+const createPaymentMethod = require("./creates/payment_method");
+module.exports = {
+  // This is just shorthand to reference the installed dependencies you have.
+  // Zapier will need to know these before we can upload.
+  version: require('./package.json').version,
+  platformVersion: require('zapier-platform-core').version,
+
+  // If you want your trigger to show up, you better include it here!
+  triggers: {},
+
+  // If you want your searches to show up, you better include it here!
+  searches: {},
+
+  // If you want your creates to show up, you better include it here!
+  creates: {
+    [createPaymentMethod.key]: createPaymentMethod
+  },
+
+  resources: {},
+};
