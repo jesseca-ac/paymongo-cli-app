a7ed0cb68f9b9b7d39b236f52655746ad7459f70
jesseca-ac <jesseca.casanare@zapier.com>
2023-10-24 12:06:16 +0800

    setup auth in index.js, test/authentication.test.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const {
+  config: authentication,
+  befores = [],
+  afters = [],
+} = require('./authentication');
+
 const createPaymentMethod = require("./creates/payment_method");
+
+
 module.exports = {
   // This is just shorthand to reference the installed dependencies you have.
   // Zapier will need to know these before we can upload.
   version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
+
+  authentication,
 
   // If you want your trigger to show up, you better include it here!
   triggers: {},
